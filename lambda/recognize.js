b20d3bbd925cf77e87bae27774a8748e7d47a692
Fernando Dingler <fdingler@amazon.com>
2018-12-06 21:17:17 -0800

    Added shim to invoke docker instead of sagemaker with environment variable

--- lambda/recognize.js
+++ lambda/recognize.js
@@ -6,6 +6,8 @@ const AWS = AWSXRay.captureAWS(require('aws-sdk'));
 const { categories } = require('./categories');
 const sageMaker = new AWS.SageMakerRuntime();
 const sageMakerEndpoint = process.env['SAGE_MAKER_ENDPOINT'];
+const dockerEndpoint = process.env['DOCKER_ENDPOINT'];
+const useSageMaker = process.env['USE_SAGE_MAKER'] || false;
 
 
 const loadImageFromUrl = async (url) => {
@@ -14,12 +16,13 @@ const loadImageFromUrl = async (url) => {
     url: url,
     responseType: 'arraybuffer',
   });
-  return (imageResponse)? imageResponse.data : null;
+  return (imageResponse)? imageResponse : null;
 };
 
 const invokeSageMaker = async (payload) => {
+  console.log('Invoking SageMaker', sageMakerEndpoint);
   const inference = await sageMaker.invokeEndpoint({
-    Body: payload,
+    Body: payload.data,
     EndpointName: sageMakerEndpoint,
     ContentType: 'application/x-image',
   }).promise();
@@ -27,6 +30,7 @@ const invokeSageMaker = async (payload) => {
   if (!inference.Body)
     return null;
 
+  console.log('SageMaker response', inference.Body);
   const classifications = JSON.parse(inference.Body.toString());
   const sorted = _.slice(classifications).sort((a, b) => b - a);
   const top = _.take(sorted, 5);
@@ -39,7 +43,28 @@ const invokeSageMaker = async (payload) => {
   });
 };
 
-const builResponse = (statusCode, body) => {
+const invokeDocker = async (payload) => {
+  console.log('Invoking docker');
+  const inference = await axios({
+    method: 'POST',
+    url: dockerEndpoint,
+    data: payload.data,
+    responseType: 'json',
+    headers: {
+      'Content-Type': payload.headers['content-type'],
+    },
+  });
+
+  console.log('Docker response', inference.data);
+  return inference.data.map(result => {
+    return {
+      name: result.class,
+      probability: result.probability,
+    }
+  });
+};
+
+const buildResponse = (statusCode, body) => {
   return {
     statusCode: statusCode,
     body: body,
@@ -53,21 +78,25 @@ const builResponse = (statusCode, body) => {
 exports.handler = async (event) => {
   console.log('event', JSON.stringify(event));
   const { imageUrl } = JSON.parse(event.body);
+  let inference = null;
+
   if (!imageUrl) {
     console.log('No imageUrl found on payload ');
-    return builResponse(400, 'No imageUrl key found on payload');
+    return buildResponse(400, 'No imageUrl key found on payload');
   }
 
   const image = await loadImageFromUrl(imageUrl);
   if (!image) {
     console.log('Unable to download image from URL');
-    return builResponse(400, 'Unable to download image from URL');
+    return buildResponse(400, 'Unable to download image from URL');
   }
 
-  const inference = await invokeSageMaker(image);
-  if (!inference) {
-    return builResponse(500, 'SageMaker model was not able to classify the image');
+  try {
+    inference = (useSageMaker) ? await invokeSageMaker(image) : await invokeDocker(image);
+  } catch (err) {
+    console.log('Failed to run inference', err);
+    return buildResponse(500, 'Failed to run inference')
   }
 
-  return builResponse(200, JSON.stringify(inference));
+  return buildResponse(200, JSON.stringify(inference));
 };
